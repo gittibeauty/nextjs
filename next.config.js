dd1e9e254837e14b38ace06c5d37925950adf02a
Zach Townsend <zachtownsend@192.168.1.15>
2020-08-07 11:46:29 +0200

    update config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   // Use the CDN in production and localhost for development.
-  assetPrefix: 'https://cdn.mydomain.com',
+  assetPrefix: 'https://gittiberlindev.myshopify.com/a/landing-page-test',
 }
