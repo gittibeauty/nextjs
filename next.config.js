a083e32a87d6aeae3ab649f63485d2d415712fc1
Zach Townsend <zachtownsend@192.168.1.15>
2020-08-07 11:56:29 +0200

    remove base path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Base path of proxy
-  basePath: 'https://gittiberlindev.myshopify.com/a/landing-page-test',
+//   basePath: 'https://gittiberlindev.myshopify.com/a/landing-page-test',
   // Use the CDN in production and localhost for development.
   assetPrefix: 'https://gittiberlindev.myshopify.com/a/landing-page-test',
 }
