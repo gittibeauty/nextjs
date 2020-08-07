f6535bb07480ad864c9786bacce25ffde38b82f3
Zach Townsend <zachtownsend@192.168.1.15>
2020-08-07 11:54:07 +0200

    add comment to force deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  // Base path of proxy
   basePath: 'https://gittiberlindev.myshopify.com/a/landing-page-test',
   // Use the CDN in production and localhost for development.
   assetPrefix: 'https://gittiberlindev.myshopify.com/a/landing-page-test',
